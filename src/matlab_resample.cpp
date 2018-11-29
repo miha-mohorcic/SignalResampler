a00b968f09de309728397039ee8800c31c3a1ac3
Miha Mohorčič <mohorcic.miha@gmail.com>
2018-11-29 15:55:27 +0100

    fix windows build

--- src/matlab_resample.cpp
+++ src/matlab_resample.cpp
@@ -1,6 +1,9 @@
 #include "matlab_resample.h"
 
 #include <cmath>
+#ifndef M_PI
+#define M_PI 3.14159265358979323846264338327950288419716939937510582097494459230781
+#endif
 #include "upfirdn.h"
 
 namespace ve::dsp::resample {
